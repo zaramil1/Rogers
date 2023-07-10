66210fd1780b17018dbbc474dda50b48d0d2a8dc
Maxwell1john <138895598+Maxwell1john@users.noreply.github.com>
2023-07-10 07:15:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_f3x425s", "template_e6mzyhu", formData)
+    emailjs.send("service_db0sxtp", "template_khmvm8g", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
