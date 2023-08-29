880b84a5ac8d630132890e4757c9f6ee13279250
Vernie1susan <139149097+Vernie1susan@users.noreply.github.com>
2023-08-29 16:52:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
