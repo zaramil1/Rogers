3fe97e9a58a533eaa03158aae900dc344fc12975
zaramil1 <144463763+zaramil1@users.noreply.github.com>
2023-09-08 07:24:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_1lo4jyu", "template_jgs02ai", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
